71f318eb920e1ce8c039410c2592d8898d53adf8
ademolashade-creator <ademolashade@gmail.com>
2026-08-28 08:49:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
-const CACHE = 'focus-flow-cache-v1';
+const CACHE = 'focus-flow-cache-v2';
 const ASSETS = ['./index.html', './styles.css', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
     self.skipWaiting();
 });
-self.addEventListener('activate', (e) => { self.clients.claim(); });
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+        )
+    );
+    self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
     e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
 });
