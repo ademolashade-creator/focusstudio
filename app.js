7e8a3a3c0f0c08e2db56cb36e347b51dab043bc8
ademolashade-creator <ademolashade@gmail.com>
2026-09-07 09:25:57 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2020,15 +2020,17 @@ function renderSingleColumn(colIndex) {
     }
     var input = document.getElementById('task-input-' + colIndex);
     if (input) setupAutosuggest(input);
-    columnEl.querySelectorAll('.task-name-input').forEach(function(el) {
-        el.style.height = 'auto';
-        el.style.height = el.scrollHeight + 'px';
-        if (el.scrollWidth > el.clientWidth + 1) {
-            el.title = el.value || el.textContent;
-        } else {
-            el.removeAttribute('title');
-        }
-    });
+    setTimeout(function() {
+        columnEl.querySelectorAll('.task-name-input').forEach(function(el) {
+            el.style.height = 'auto';
+            el.style.height = el.scrollHeight + 'px';
+            if (el.scrollWidth > el.clientWidth + 1) {
+                el.title = el.value || el.textContent;
+            } else {
+                el.removeAttribute('title');
+            }
+        });
+    }, 0);
     restoreScrollPositions();
 }
 
